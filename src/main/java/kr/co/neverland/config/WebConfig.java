aa0b5875069a733176f55fc324a7c7a00e8e640c
superbono <choiar@pincar.co.kr>
2022-01-27 11:29:50 +0900

    backend Swagger dependencies / cors 추가

---
+++ src/main/java/kr/co/neverland/config/WebConfig.java
@@ -0,0 +1,23 @@
+package kr.co.neverland.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true)
+                .maxAge(180)
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/index.html**").addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
