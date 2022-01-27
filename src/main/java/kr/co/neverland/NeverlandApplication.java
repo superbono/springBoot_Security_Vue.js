9f8277b9ae4431cec90247245d85e1dbf5a8b85e
superbono <choiar@pincar.co.kr>
2022-01-27 09:51:08 +0900

    first commit

---
+++ src/main/java/kr/co/neverland/NeverlandApplication.java
@@ -0,0 +1,13 @@
+package kr.co.neverland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeverlandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeverlandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/neverland/NeverlandApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.neverland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeverlandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
