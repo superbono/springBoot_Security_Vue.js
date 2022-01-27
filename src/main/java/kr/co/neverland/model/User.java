b4a0b02121c27ed97b8ee61e2857e886481fb4f6
superbono <choiar@pincar.co.kr>
2022-01-27 10:39:45 +0900

    backend Entity 생성 _ JPA

---
+++ src/main/java/kr/co/neverland/model/User.java
@@ -0,0 +1,42 @@
+package kr.co.neverland.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(length = 100)
+    private String username;
+    @Column(length = 100)
+    private String password;
+    @Column(length = 100)
+    private String nickname;
+    @Column(length = 100)
+    private String roles;
+
+    public List<String> getRoleList() {
+        if(this.roles.length() > 0) {
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+
+    @Builder
+    public User(String username, String password, String nickname) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+    }
+}
