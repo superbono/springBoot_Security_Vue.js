5893935d429773b3bc0399f587a8e17cb79bde3d
superbono <choiar@pincar.co.kr>
2022-01-28 11:30:07 +0900

    frontend 로그인페이지 디자인 적용_4

--- src/frontend/src/components/LoginForm.vue
+++ src/frontend/src/components/LoginForm.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="contents" style="border: 1px solid #fff; width: 400px; opacity: .7;
-                               height: 400px; margin: 16% auto; background: #fff; border-radius: 30px;
-                               box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">
+                               height: 400px; margin: 4% auto; background: #fff; border-radius: 30px;">
     <div class="form-wrapper form-wrapper-sm" style="margin-top: -2%">
       <div style="text-align: center">
         <div class="loginTitle">
@@ -9,35 +8,71 @@
         </div>
       </div>
       <form @submit.prevent="submitForm" class="form" style="text-align: center">
-        <div style="margin-top: -60px;">
+        <div style="margin-top: -45px;">
           <label for="username"></label>
-          <input id="username" type="text" v-model="username" style="border: 1px solid #000; border-radius: 5px; width: 70%; height: 35px;
+          <input id="username" type="text" v-model="username" style="border: 1px solid #D1D5D8; width: 85%; height: 35px;
                                                                       background: #fff;" placeholder="이메일을 입력하세요" />
-          <p class="validation-text">
-            <span class="warning" v-if="!isUsernameValid && username">
-              Please enter an email address
-            </span>
-          </p>
         </div>
         <div>
           <label for="password"></label>
-          <input id="password" type="password" v-model="password" style="border: 1px solid #000; border-radius: 5px; width: 70%; height: 35px;
-                                                                         background: #fff; margin-top: -80px;" placeholder="비밀번호를 입력하세요" />
+          <input id="password" type="password" v-model="password" style="border: 1px solid #D1D5D8; width: 85%; height: 35px;
+                                                                         background: #fff; margin-top: 10px;" placeholder="비밀번호를 입력하세요" />
+          <p class="validation-text" style="margin-top: 8px; margin-right: 105px; font-size: 11px; color: brown">
+            <span class="warning" v-if="!isUsernameValid && username">
+              올바른 형식이 아닙니다. 확인 후 다시 입력해주세요.
+            </span>
+          </p>
         </div>
-        <div style="margin-top: 20px;">
+        <p style="font-size: 11px; margin-right: 140px; text-decoration: underline">
+          <a @click="joinMove" style="cursor: pointer; ">아직 회원이 아니신가요? 회원가입 하러가기</a>
+        </p>
+        <div style="margin-top: 30px;">
           <button
               :disabled="!isUsernameValid || !password"
               type="submit"
               class="btn"
-              style="width: 300px; height: 42px; background: #e1eff6; border: 1px solid #d0e1d4; margin-top: -5px;"
+              style="width: 340px; height: 42px; background: #fff; border: 1px solid #d0e1d4; margin-top: -5px; font-weight: bold;"
           >
             로그인
           </button>
+        </div>
+        <div style="margin-top: 10px;">
           <button
-              type="reset" style="width: 300px; height: 42px; background: #e1eff6; border: 1px solid #d0e1d4; margin-top: 5px;"
+              :disabled="!isUsernameValid || !password"
+              type="submit"
+              class="btn"
+              style="width: 340px; height: 42px; background: #fff; border: 1px solid #d0e1d4;
+                     margin-top: -5px; font-weight: bold; background: #3b5998; color: #fff"
           >
-            취소
+            페이스북 아이디로 로그인
           </button>
+        </div>
+        <div style="margin-top: 10px;">
+          <button
+              :disabled="!isUsernameValid || !password"
+              type="submit"
+              class="btn"
+              style="width: 340px; height: 42px; background: #fff; border: 1px solid #d0e1d4;
+                     margin-top: -5px; font-weight: bold; background: #2DB400; color: #fff"
+          >
+            네이버 아이디로 로그인
+          </button>
+        </div>
+        <div style="margin-top: 10px;">
+          <button
+              :disabled="!isUsernameValid || !password"
+              type="submit"
+              class="btn"
+              style="width: 340px; height: 42px; background: #E74C3C; border: 1px solid #d0e1d4;
+                     margin-top: -5px; font-weight: bold; color: #fff"
+          >
+            구글 아이디로 로그인
+          </button>
+<!--          <button-->
+<!--              type="reset" style="width: 300px; height: 42px; background: #e1eff6; border: 1px solid #d0e1d4; margin-top: 5px;"-->
+<!--          >-->
+<!--            취소-->
+<!--          </button>-->
       </div>
       </form>
       <p class="log">{{ logMessage }}</p>
@@ -85,6 +120,9 @@ export default {
       } finally {
         this.initForm();
       }
+    },
+    joinMove() {
+      this.$router.push("/join");
     },
     initForm() {
       this.username = '';
