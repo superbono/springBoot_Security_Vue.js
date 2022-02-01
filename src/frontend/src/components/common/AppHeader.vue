e3f3ff4190f59fb7458778348afc1c13b742f1df
superbono <choiar@pincar.co.kr>
2022-02-01 17:18:30 +0900

    frontend 메인페이지 스타일링

--- src/frontend/src/components/common/AppHeader.vue
+++ src/frontend/src/components/common/AppHeader.vue
@@ -1,40 +1,28 @@
 <template>
   <header>
     <template v-if="isUserLogin">
-      <div>
-        <b-navbar toggleable="lg" type="dark" style="background: #2C3E50; height: 70px;">
-          <b-navbar-brand href="/main" style="margin-left: 8px;">
-            <img src="@/assets/img/logo_white.png" style="width: 140px; height: 120px;">
-          </b-navbar-brand>
-
-          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-          <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav>
-              <b-nav-item href="#">조회</b-nav-item>
-              <b-nav-item href="#">작성</b-nav-item>
-            </b-navbar-nav>
-
-            <!-- Right aligned nav items -->
-            <b-navbar-nav class="ml-auto">
-
-              <b-nav-item-dropdown right style="margin-left: 1000px;">
-                <!-- Using 'button-content' slot -->
-                <template #button-content>
-                  <em>User</em>
-                </template>
-                <b-dropdown-item href="#">Profile</b-dropdown-item>
-                <b-dropdown-item href="#">
-                  <a href="javascript:;" @click="logoutUser" class="logout-button"
-                     style="text-decoration: none; color: #000">
-                    로그아웃
-                  </a>
-                </b-dropdown-item>
-              </b-nav-item-dropdown>
-            </b-navbar-nav>
-          </b-collapse>
-        </b-navbar>
-      </div>
+      <a href="#contents" class="skip">본문바로가기</a>
+      <header>
+        <h1><a href="#a">네버랜드</a></h1>
+        <h2 class="hide">대메뉴</h2>
+        <nav class="lnb">
+          <ul>
+            <li><a href="#a"><span>홈</span></a></li>
+            <li><a href="#a"><span>게시판</span></a></li>
+            <li><a href="#a"><span>네버랜드</span></a></li>
+            <li><a href="#a"><span>마이페이지</span></a></li>
+            <li><a href="#a"><span>공지사항</span></a></li>
+          </ul>
+        </nav>
+        <h2 class="hide">관련서비스</h2>
+        <nav class="spot">
+          <ul>
+            <li><a href="#a">프로젝트소개</a></li>
+            <li><a href="#a">프로필</a></li>
+            <li><a href="#a" @click="logoutUser">로그아웃</a></li>
+          </ul>
+        </nav>
+      </header>
     </template>
     <template v-else>
 
@@ -68,6 +56,7 @@ export default {
 </script>
 
 <style scoped>
+
 /*.username {*/
 /*  color: white;*/
 /*}*/
--- src/frontend/src/views/MainPage.vue
+++ src/frontend/src/views/MainPage.vue
@@ -1,6 +1,110 @@
 <template>
   <div>
-    <h2>메인페이지</h2>
+    <section class="visual slide">
+      <div class="con">
+        <div class="slide">
+          <div class="txt">
+            당신이 생각하는<br>
+            NEVERLAND
+            <p>가장 멋진 네버랜드를 만들어주세요</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="product">
+      <h2 class="hide">제품정보</h2>
+      <ul>
+        <li><a href="#a">홈</a></li>
+        <li><a href="#a">작성</a></li>
+        <li><a href="#a">조회</a></li>
+        <li><a href="#a">프로필</a></li>
+        <li><a href="#a">네버랜드</a></li>
+        <li><a href="#a">연락</a></li>
+      </ul>
+    </section>
+    <div class="section">
+      <h2>NEVERLAND</h2>
+      <p>다양한 네버랜드를 구경하세요</p>
+      <input type="radio" name="slide" id="slide01" checked>
+      <input type="radio" name="slide" id="slide02">
+      <input type="radio" name="slide" id="slide03">
+      <input type="radio" name="slide" id="slide04">
+      <div class="slidewrap">
+        <ul class="slidelist">
+          <!-- 슬라이드 영역 -->
+          <li class="slideitem">
+            <a>
+              <div class="textbox">
+                <h3>첫번째 슬라이드</h3>
+                <p>첫번째 슬라이드 입니다.</p>
+              </div>
+              <img src="@/assets/images/img6.jpg">
+            </a>
+          </li>
+          <li class="slideitem">
+            <a>
+
+              <div class="textbox">
+                <h3>두번째 슬라이드</h3>
+                <p>두번째 슬라이드 입니다.</p>
+              </div>
+              <img src="@/assets/images/img6.jpg">
+            </a>
+          </li>
+          <li class="slideitem">
+            <a>
+
+              <div class="textbox">
+                <h3>세번째 슬라이드</h3>
+                <p>세번째 슬라이드 입니다.</p>
+              </div>
+              <img src="@/assets/images/img6.jpg">
+            </a>
+          </li>
+          <!-- 좌,우 슬라이드 버튼 -->
+          <div class="slide-control">
+            <div>
+              <label for="slide03" class="left"></label>
+              <label for="slide02" class="right"></label>
+            </div>
+            <div>
+              <label for="slide01" class="left"></label>
+              <label for="slide03" class="right"></label>
+            </div>
+            <div>
+              <label for="slide02" class="left"></label>
+              <label for="slide01" class="right"></label>
+            </div>
+          </div>
+
+        </ul>
+        <!-- 페이징 -->
+        <ul class="slide-pagelist">
+          <li><label for="slide01"></label></li>
+          <li><label for="slide02"></label></li>
+          <li><label for="slide03"></label></li>
+        </ul>
+      </div>
+    </div>
+    <section class="notice">
+      <div class="clear">
+        <div class="top clear">
+          <h2>WHAT IS NEVERLAND?</h2>
+          <ul>
+            <li><a href="#a">창작하는<span>당신이 생각하는 플랫폼</span></a></li>
+            <li><a href="#a">공유하는<span>우리가 생각하는 플랫폼</span></a></li>
+            <li><a href="#a">자유로운<span>가까운 한솔홈데코 매장 확인</span></a></li>
+            <li><a href="#a">특별한<span>한솔의 행복한 삶의 공간 이야기</span></a></li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <footer>
+      <div class="btm">
+        <p>네버랜드</p>
+        <address>Copyright 2022 NEVERLAND All rights reserved.</address>
+      </div>
+    </footer>
   </div>
 </template>
 
